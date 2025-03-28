7356cf0b80ac178552952b50ba4707190424f7b3
donnillionaire <donaldodhiambo09@gmail.com>
2025-03-28 12:09:23 +0300

    alembic changes

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.model import Base
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Odhiambo10@127.0.0.1:5432/journal"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Odhiambo10@127.0.0.1:5432/journal"
+SQLALCHEMY_DATABASE_URL = os.getenv("REMOTE_DATABASE_URL")  # Use an environment variable for security
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
